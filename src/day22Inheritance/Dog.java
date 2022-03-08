351e4a37452f3b18f0df1675cf078a87bb2cadb2
Suleyman Alptekin <slptkn@yahoo.com>
2022-03-07 21:57:23 -0500

    Inheritance

---
+++ src/day22Inheritance/Dog.java
@@ -0,0 +1,14 @@
+package day22Inheritance;
+
+public class Dog extends Mammals{ //The class name before the "extends" keyword is the child class
+                                 //The class name after the "extends" keyword is the parent class
+
+    public void bark(){
+        System.out.println("Dogs bark...");
+    }
+
+    public Dog(){
+        System.out.println("Dog constructor");
+    }
+
+}
---
+++ src/day22Inheritance/Mammals.java
@@ -0,0 +1,12 @@
+package day22Inheritance;
+
+public class Mammals extends Animal{
+
+    public void feedBabyWithMilk(){
+        System.out.println("Mammals feed their baby with milk...");
+    }
+
+    public Mammals(){
+        System.out.println("Mammals constructor");
+    }
+}
